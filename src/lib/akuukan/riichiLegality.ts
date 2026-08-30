f00bc989fa68a12c8ca7c7348fb2d00db993b416
takugo20 <takugo20@me.com>
2026-08-30 16:35:17 +0900

    Create riichiLegality.ts

---
+++ src/lib/akuukan/riichiLegality.ts
@@ -0,0 +1,56 @@
+import {
+  isAkuukanPlayerSkillEquipped
+} from "./equipment";
+import {
+  isAkuukanSourceDisabled
+} from "./state";
+import type {
+  AkuukanGameState
+} from "./types";
+import {
+  isEnemyAbilityEnabled
+} from "./winningEvaluationEnemyAbilityAdjustments";
+
+export type AkuukanRiichiOwner =
+  | "player"
+  | "selectedEnemy"
+  | "normalOpponent";
+
+export interface AkuukanOpenRiichiCheckInput {
+  readonly akuukan: AkuukanGameState;
+  readonly owner: AkuukanRiichiOwner;
+}
+
+function isPlayerOpenRiichiEnabled(
+  akuukan: AkuukanGameState
+): boolean {
+  return (
+    isAkuukanPlayerSkillEquipped(
+      akuukan,
+      "2-7"
+    ) &&
+    !isAkuukanSourceDisabled(
+      akuukan,
+      "player-skill:2-7"
+    )
+  );
+}
+
+export function isAkuukanOpenRiichiAllowed(
+  input: AkuukanOpenRiichiCheckInput
+): boolean {
+  if (input.owner === "player") {
+    return isPlayerOpenRiichiEnabled(
+      input.akuukan
+    );
+  }
+
+  if (input.owner === "selectedEnemy") {
+    return isEnemyAbilityEnabled(
+      input.akuukan,
+      "E-14"
+    );
+  }
+
+  return false;
+}
