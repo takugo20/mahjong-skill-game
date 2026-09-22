caacddad017f113a6677591aacb5d041dbded90b
takugo20 <274753337+takugo20@users.noreply.github.com>
2026-09-22 17:53:27 +0900

    Prepare Capacitor iOS development environment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.takugo20.akuukanmahjong',
+  appName: '亜空間麻雀',
+  webDir: 'dist'
+};
+
+export default config;
